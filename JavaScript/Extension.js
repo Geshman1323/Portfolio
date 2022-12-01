a213bbd61d9ce27a1628e9473c020f46f6a11444
Geshman Shakeel <113760897+Geshman1323@users.noreply.github.com>
2022-12-01 07:29:53 -0800

    Delete Extension.js

--- JavaScript/Extension.js
+++
@@ -1,6 +0,0 @@
-export const open = (element, Link) => {
-    let e = document.querySelector(element);
-    return e.addEventListener('click' , () => {
-        document.location.href = Link;
-    })
-}
